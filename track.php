56ff69279f6f2cf432ce0fec1f5de522cd974187
Jhon Adams <jhon@jhonadams.net>
2016-04-02 15:11:54 -0700

    Bugfix + Data Validation

--- track.php
+++ track.php
@@ -1,11 +1,15 @@
 <?php
 header('Access-Control-Allow-Origin: *');  
 echo "OK";
-require_once "config.php"
-if(empty($_GET['id'])){ die(); }
+require_once "config.php";
+
+if(empty(@$_GET['id'])){ die(); }
 
 // If the user is banned, don't let them submit data
-if(in_array($_SERVER['REMOTE_ADDR'],$banned)){ die(); }
+if(in_array(@$_SERVER['REMOTE_ADDR'],$banlist)){ die(); }
+
+// Basic data validation
+if (intval(@$_GET['count']) != (intval(@$_GET['gr'])+intval(@$_GET['st'])+intval(@$_GET['ab'])+intval(@$_GET['nv']))) { die(); }
 
 $last_user_id = $database->insert("track", [
 	"room" => $_GET['id'],
