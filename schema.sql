2538524263b6dba72af0e5ecdd53a18ebb130cc0
Jhon Adams <jhon@jhonadams.net>
2016-04-04 19:31:17 -0700

    Split DB into cache and storage

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: mysql.monstrouspeace.com
--- Generation Time: Apr 02, 2016 at 04:18 PM
+-- Generation Time: Apr 04, 2016 at 07:30 PM
 -- Server version: 5.6.25-log
 -- PHP Version: 7.0.4
 
@@ -41,6 +41,27 @@ CREATE TABLE `track` (
   `ip` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `track_storage`
+--
+
+CREATE TABLE `track_storage` (
+  `id` int(11) NOT NULL,
+  `guid` varchar(36) NOT NULL,
+  `room` char(10) NOT NULL,
+  `count` int(6) NOT NULL,
+  `grow` int(6) NOT NULL,
+  `stay` int(6) NOT NULL,
+  `abandon` int(6) NOT NULL,
+  `novote` int(6) NOT NULL,
+  `formation` int(11) NOT NULL,
+  `reap` int(11) NOT NULL,
+  `time` int(11) NOT NULL,
+  `ip` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 --
 -- Indexes for dumped tables
 --
@@ -55,6 +76,15 @@ ALTER TABLE `track`
   ADD KEY `guid` (`guid`);
 
 --
+-- Indexes for table `track_storage`
+--
+ALTER TABLE `track_storage`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `room` (`room`),
+  ADD KEY `count` (`count`),
+  ADD KEY `guid` (`guid`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -62,6 +92,11 @@ ALTER TABLE `track`
 -- AUTO_INCREMENT for table `track`
 --
 ALTER TABLE `track`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `track_storage`
+--
+ALTER TABLE `track_storage`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
